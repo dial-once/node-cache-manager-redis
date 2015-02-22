24a6e5dd81dff303d64eae3965e8b21a334f8c7f
Julien Kernec'h <julien.kernech@outlook.com>
2015-02-22 12:50:40 +0100

    Formatting code

--- gruntfile.js
+++ gruntfile.js
@@ -2,24 +2,22 @@
 
 module.exports = function (grunt) {
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-		// Make sure code styles are up to par and there are no obvious mistakes
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc',
-				reporter: require('jshint-stylish')
-			},
-			all: [
-				'gruntfile.js',
-				'index.js'
-			]
-		}
-	});
+  grunt.initConfig({
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'gruntfile.js',
+        'index.js'
+      ]
+    }
+  });
 
-	grunt.registerTask('default', [
-		'jshint'
-	]);
+  grunt.registerTask('default', ['jshint']);
 };
