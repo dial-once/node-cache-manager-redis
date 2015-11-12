d6a463edaf72e129a0c4f7b6331c15dc42c4cf28
Julien Kernec'h <julien.kernech@outlook.com>
2015-11-12 18:23:53 +0100

    Add tests : set

--- spec/lib/redis-store-spec.js
+++ spec/lib/redis-store-spec.js
@@ -13,3 +13,35 @@ beforeAll(function() {
     ttl: config.redis.ttl
   });
 });
+
+describe('set', function() {
+  it('should store a value without ttl', function(done){
+    redisCache.set('foo', 'bar', function(err) {
+      expect(err).toBe(null);
+      done();
+    });
+  });
+
+  it('should store a value with a specific ttl', function(done){
+    redisCache.set('foo', 'bar', config.redis.ttl, function(err) {
+      expect(err).toBe(null);
+      done();
+    });
+  });
+
+  it('should store a value with a infinite ttl', function(done){
+    redisCache.set('foo', 'bar', 0);
+    done();
+  });
+
+  it('should store a value without callback', function(done){
+    redisCache.set('foo', 'bar');
+    done();
+  }); 
+
+  it('should not store an invalid value', function(done){
+    redisCache.set('foo1', null);
+    redisCache.set('foo2', undefined);
+    done();
+  });
+});
